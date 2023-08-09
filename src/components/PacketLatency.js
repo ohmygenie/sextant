6534b9c4949099225da70ef82815f5eb97491f74
Oh My Genie <jomarnuguid0830@gmail.com>
2023-08-09 16:33:53 +1000

    commit

--- src/components/PacketLatency.js
+++ src/components/PacketLatency.js
@@ -2,10 +2,15 @@ import React, { useEffect, useState } from 'react';
 
 function PacketLatency() {
   const [latency, setLatency] = useState(null);
+  const [status, setStatus] = useState('Connecting...');
   const websocketURL = "ws://localhost:55455";
 
   useEffect(() => {
     const ws = new WebSocket(websocketURL);
+
+    ws.onopen = () => {
+      setStatus('Connected. Waiting for packets...');
+    };
 
     ws.onmessage = (event) => {
       const receivedData = JSON.parse(event.data);
@@ -16,7 +21,16 @@ function PacketLatency() {
       }
     };
 
+    ws.onclose = (event) => {
+      if (event.wasClean) {
+        setStatus(`Connection closed cleanly, code=${event.code}, reason=${event.reason}`);
+      } else {
+        setStatus('Connection died unexpectedly.');
+      }
+    };
+
     ws.onerror = (error) => {
+      setStatus('WebSocket encountered an error.');
       console.error("WebSocket Error: ", error);
     };
 
@@ -31,7 +45,7 @@ function PacketLatency() {
       {latency !== null ? (
         <p>Packet Latency from Pylon: {latency} milliseconds</p>
       ) : (
-        <p>Waiting for packets...</p>
+        <p>{status}</p>
       )}
     </div>
   );
