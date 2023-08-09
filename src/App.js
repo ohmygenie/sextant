73ec27ee748fc347d1f9444551fc53d98c2a444a
Oh My Genie <jomarnuguid0830@gmail.com>
2023-08-09 15:31:46 +1000

    commit ammend

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import AddressRequest from './components/AddressRequest'; // Ensure you've imported this
+import Banner from './components/Banner';
+import Exhibit from './components/Exhibit';
 
 function App() {
   return (
     <div className="App">
+      <Banner />
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Exhibit title="React Exhibit">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </Exhibit>
+
+        <Exhibit title="IPv4 Address">
+          <AddressRequest ipVersion="ipv4" />
+        </Exhibit>
+        
+        <Exhibit title="IPv6 Address">
+          <AddressRequest ipVersion="ipv6" />
+        </Exhibit>
       </header>
     </div>
   );
